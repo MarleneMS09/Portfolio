c23a432c47610c79aeba4adb64baa184d95a6099
MarleneMS09 <marlenemsanchez05@gmail.com>
2023-09-07 20:44:58 -0600

    start project

---
+++ head..php
@@ -0,0 +1,5 @@
+<?php ?>
+
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfoliot</title>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?php require 'head.php' ?>
+</head>
+<header>
+    <?php require 'header.php' ?>
+</header>
+<body>
+    
+</body>
+</html>
